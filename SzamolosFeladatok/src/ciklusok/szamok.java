4547aa6c26a5d26e8b9e525ad25c7ef5813e58e9
Mancska <franyokrisz@gmail.com>
2023-03-02 16:04:23 +0100

    mind 3

--- SzamolosFeladatok/src/ciklusok/szamok.java
+++ SzamolosFeladatok/src/ciklusok/szamok.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ciklusok;
 
-/**
- *
- * @author Krisz
- */
 public class szamok {
+    public static void main(String[] args) {
+        for (int z = 2; z < 5; z++) {
+            for (int j = 2; j < 4; j++) {
+            for (int i = 2; i < 5; i++) {
+            System.out.println(z+""+j+""+i);
+            
+        }
+        }
+        
+   
+        }
+        
+    }
     
 }
